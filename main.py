2aee12106f5e5474265537c5faa5f02ed4501382
GUILHEM_FAURE <guilhemfaure@outlook.fr>
2023-07-15 15:23:19 +0200

    FEAT: accept different masses input

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from lib.animate import SolveAnim
 
 def main():
     solver_anim = SolveAnim(
-        config="default",
+        config="default",  # different_masses
         movie_path="outputs/default_movie.mp4"
     )
     solution = solver_anim.solve()
